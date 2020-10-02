7ae1cf8ae13fe85ddf9bf3de0c18afbefd684616
Keith JL <keithjl@mit.edu>
2020-10-01 22:36:24 -0400

    exported callable functions in main module.

--- src/TopOptTools.jl
+++ src/TopOptTools.jl
@@ -5,4 +5,13 @@ pyplot(framestyle = :box, legend = false, color = :black)
 include("fem2d.jl")
 include("opt.jl")
 
+#Export data types
+export node, element
+
+#Export main solver functions
+export fem2d_solver, fem2d_solver_modified
+
+#Export Topology optimization method
+export minimize_unconstrained
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using TopOptTools
 using Test
 
+r1 = node(1.0, 3.4, true, true)
+
 @testset "TopOptTools.jl" begin
     # Write your tests here.
+    
 end
